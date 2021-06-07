fcfa32240aa8be44e3a96fd8791406ee75a4aeec
Kristien Kellens <kristien.kellens@gmail.com>
2021-06-07 11:46:21 +0200

    added throws counter

--- modules/game.js
+++ modules/game.js
@@ -8,6 +8,8 @@ export default class Game {
         this.rollDiceBtn = document.getElementById("btn-roll");
         this.instructionsTxt = document.querySelector(".instructions");
         this.diceArea = document.querySelector(".dice-area");
+        this.throwsSpan = document.querySelector(".throws");
+        this.throws = 3;
         this.hasStarted = false;
         this.diceArr = [];
 
@@ -15,30 +17,38 @@ export default class Game {
 
     start() {
         if (this.hasStarted) { window.location.reload(); };
-        // display instructions
+        // display instructions, number of throws
         this.instructionsTxt.style.display = "block";
+        this.throwsSpan.innerText = this.throws;
+
         //make roll dice button active
         this.hasStarted = true;
     }
 
     createDice() {
-        //on first throw
-        if (this.diceArr.length === 0) {
-            //make 5 dice
-            for (let i = 0; i < 5; i++) {
-                //console.log(i);
-                this.diceArr.push(new Dice(i));
+        //you can only roll dice for three throws
+        if (this.throws <= 3 && this.throws > 0) {
+            //on first throw
+            if (this.diceArr.length === 0) {
+                //make 5 dice
+                for (let i = 0; i < 5; i++) {
+                    //console.log(i);
+                    this.diceArr.push(new Dice(i));
+                }
+
+                //roll each dice
+                this.diceArr.forEach(dice => { dice.roll(); });
+            } else {
+                this.diceArr.forEach(dice => {
+                    this.keepLockedDice(dice);
+                })
             }
 
-            //roll each dice
-            this.diceArr.forEach(dice => {
-                dice.roll();
-            });
-        } else {
-            this.diceArr.forEach(dice => {
-                this.keepLockedDice(dice);
-            })
+            //count throws
+            this.throws -= 1;
+            this.throwsSpan.innerText = this.throws;
         }
+
     }
 
     keepLockedDice(dice) {
