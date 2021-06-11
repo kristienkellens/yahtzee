4f196fcf001e47538d56c7d9e770900974f5f812
Kristien Kellens <kristien.kellens@gmail.com>
2021-06-11 12:03:32 +0200

    second attempt small straight

--- modules/scorecard.js
+++ modules/scorecard.js
@@ -98,7 +98,7 @@ export default class Scorecard {
 
 
     isYathzee() {
-        if (new Set(this.diceValuesArr).size === 1) {
+        if ((new Set(this.diceValuesArr).size === 1)) {
             document.getElementById("yahtzee").innerText = 50;
         } else {
             document.getElementById("yahtzee").innerText = 0;
@@ -118,21 +118,69 @@ export default class Scorecard {
     }
 
     isSmallStraight() {//there are three possible small straights: {1, 2, 3, 4}, {2, 3, 4, 5} and {3, 4, 5, 6}, first number will always be 1, 2 or 3
+        //small straight = 4 consecutive nrs
+        let consecutiveCounter = 1; //if 4, then we have a straight
+
+        if (Object.keys(this.occurences)[0] <= 3 && new Set(this.diceValuesArr).size >= 4) {
+
+            for (let i = 0; i < Object.keys(this.occurences).length - 1; i++) {
+                //loop through all keys
+
+                //if key[i+1] == key[i] + 1, counter++;
+                if (parseInt(Object.keys(this.occurences)[i + 1]) === (parseInt(Object.keys(this.occurences)[i]) + 1)) {
+
+                    consecutiveCounter++;
+                    //console.log("match", consecutiveCounter);
+                } else {
+                    //reset consecutive counter to 0
+                    consecutiveCounter = 1;
+                    //console.log("no match", consecutiveCounter);
+                }
+            }
+
+        } else {
+            console.log("not a straight: either first key is +3 or set size is below 4");
+            document.getElementById("sm-straight").innerText = 0;
+            document.getElementById("sm-straight").classList.add("zero");
+        }
+
+        console.log(consecutiveCounter);
+        if (consecutiveCounter === 4) { //is a small straight
+            document.getElementById("sm-straight").innerText = 30;
+        }
+
+
+
+        /*
+        //ISSUE: if first key is 1, but you do have a straight with 3,4,5,6
         let isKeyOccurences = false;
         if (Object.keys(this.occurences)[0] <= 3) {
+
             console.log("first key is 1, 2 or 3");
-            let firstKey = parseInt(Object.keys(this.occurences)[0]);
-            for (let i = 0; i < 3; i++) {
+            for (let i = 0; i < Object.keys(this.occurences).length; i++) {
+                console.log("index is", i, "key value is", Object.keys(this.occurences)[i], "next value should be", i + parseInt(Object.keys(this.occurences)[i]), "but next key value is", Object.keys(this.occurences)[i + 1]);
+
+
+
+                // if Object.keys(this.occurences)[i + 1] === undefined, end of loop?
+
+
+            }
+
+
+            /*for (let i = 0; i < 3; i++) {
+                let keyValue = parseInt(Object.keys(this.occurences)[i+1])
                 //console.log("first key + i", firstKey, i, firstKey + i + 1);
                 if ((firstKey + i + 1) in this.occurences) {
-                    isKeyOccurences = true;
+                    isKeyOccurences = true; //counter iso isKeyoccurences?
                     console.log(firstKey + i + 1, isKeyOccurences);
                 } else {
+                    //what if straight starts later
                     isKeyOccurences = false;
                     console.log(firstKey + i + 1, isKeyOccurences);
                     break
                 }
-            }
+
         } else {
             console.log("first key is more than 3");
             document.getElementById("sm-straight").innerText = 0;
@@ -145,7 +193,7 @@ export default class Scorecard {
             document.getElementById("sm-straight").innerText = 0;
             document.getElementById("sm-straight").classList.add("zero");
 
-        }
+        }*/
 
 
         /*
@@ -163,6 +211,7 @@ export default class Scorecard {
     isLargeStraight() { //can only be {1, 2, 3, 4, 5} or {2, 3, 4, 5, 6}
         const diceSet = (new Set(this.diceValuesArr));
         if ((diceSet.size === 5) && (!diceSet.has(1) || !diceSet.has(6))) {
+            console.log("is large straight");
             document.getElementById("l-straight").innerText = 40;
         } else {
             document.getElementById("l-straight").innerText = 0;
